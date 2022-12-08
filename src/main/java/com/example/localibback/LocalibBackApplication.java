436df5bc7593af91e51f2b2cfdb0899feab73a52
FlorianFlouquet <florian.flouquet2@hotmail.com>
2022-12-08 08:53:54 +0100

    initializing project

---
+++ src/main/java/com/example/localibback/LocalibBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.localibback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalibBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalibBackApplication.class, args);
+	}
+
+}
