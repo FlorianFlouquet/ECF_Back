ee48de75eec4515de224e5feb0de5bedbf683257
FlorianFlouquet <florian.flouquet2@hotmail.com>
2022-12-08 09:25:00 +0100

    feat: can now do basics call request such as get all vehicules, get a specific one, post one or delete one

--- src/main/java/com/example/localibback/vehicules/Vehicule.java
+++ src/main/java/com/example/localibback/vehicules/Vehicule.java
@@ -1,2 +1,23 @@
-package com.example.localibback.vehicules;public class Vehicule {
+package com.example.localibback.vehicules;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Vehicule {
+    @Id
+    private String id;
+    private String brand;
+    private String model;
+    private String state;
+    private String license_number;
+    private String type;
+    private Double price;
+    private boolean available;
 }
--- src/main/java/com/example/localibback/vehicules/VehiculeRepository.java
+++ src/main/java/com/example/localibback/vehicules/VehiculeRepository.java
@@ -1,2 +1,6 @@
-package com.example.localibback.vehicules;public interface VehiculeRepository {
+package com.example.localibback.vehicules;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface VehiculeRepository extends MongoRepository<Vehicule, String> {
 }
